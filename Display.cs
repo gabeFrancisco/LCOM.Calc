f1b110144bff441a962c4845b60e948ec49ed4b1
gabeFrancisco <gabegit2@gmail.com>
2025-10-06 14:32:00 -0300

    created InputValues method on Display class

--- Display.cs
+++ Display.cs
@@ -6,5 +6,22 @@ namespace LCOM.Calc
         {
             Console.WriteLine($"\nThe result of LCOM equation is: {value}\n");
         }
+
+        public static dynamic InputValues()
+        {
+            int attributes, methods;
+
+            Console.Write("Enter the number of attributes: ");
+            attributes = int.Parse(Console.ReadLine()!);
+
+            Console.Write("\nEnter the number of methods: ");
+            methods = int.Parse(Console.ReadLine()!);
+
+            return new
+            {
+                attributes,
+                methods
+            };
+        }
     }
 }
